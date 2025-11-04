311eef0215cff1d05b3296eef39225f83a203513
rasgaxota572 <rasgaxota575@gmail.com>
2025-11-04 05:35:37 -0300

    Create build_kernel.sh

---
+++ build_kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export CC=clang
+export LLVM=1
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+make guamp_defconfig
+make -j$(nproc) CC=clang LLVM=1
